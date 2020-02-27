60ba15844fdaea7a59aa6111623fe08c5a6838eb
Prashant Shukla <shukla.prashant689@gmail.com>
2020-02-27 23:57:55 +0530

    Create notify.py

---
+++ notify.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib      # simple mail transfer protocol
+
+url = 'https://www.amazon.in/boAt-BassHeads-100-Headphones-Black/dp/B071Z8M4KX/ref=sr_1_4?keywords=boat&qid=1582823584&sr=8-4'
+
+headers = {
+    "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'
+}
+
+def check_price():
+    page = requests.get(url, headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find(id='productTitle').get_text()
+    price = soup.find(id='priceblock_ourprice').get_text()
+    converted_price = float(price[2:5])
+
+    if converted_price < 500:
+        send_mail()
+    print(converted_price)
+    print(title.strip())
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('sender@mail', 'password')         # need to allow less secure app for google
+    subject = 'Price fell down !!!'
+    body = 'Check the link https://www.amazon.in/boAt-BassHeads-100-Headphones-Black/dp/B071Z8M4KX/ref=sr_1_4?keywords=boat&qid=1582823584&sr=8-4'
+
+    msg = f"Subject: {subject}\n\n {body}"
+
+    server.sendmail(
+        'sender@mail',
+        'receiver@mail',
+        msg
+    )
+
+    print('Email has been sent.')
+
+    server.quit()
+
+check_price()
